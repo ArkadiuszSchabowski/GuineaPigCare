7625019ad69cce93ce8bfa39335c31ea21a00631
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-08-04 20:36:09 +0200

    Update Entity User - All fields are required, apply the same migration

--- GuineaPigCare.Server/Models/UserDto.cs
+++ GuineaPigCare.Server/Models/UserDto.cs
@@ -3,10 +3,10 @@
     public class UserDto
     {
         public string Email { get; set; }
-        public string? Name { get; set; }
-        public string? Surname { get; set; }
-        public string? City { get; set; }
-        public string? PostalCode { get; set; }
-        public DateOnly? DateOfBirth {  get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public DateOnly DateOfBirth {  get; set; }
     }
 }
