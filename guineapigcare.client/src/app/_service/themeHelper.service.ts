a9654b53848af859faa4593bbe28f60875cb909a
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-08-31 16:11:29 +0200

    add preloadsBackgrounds method when app started

--- guineapigcare.client/src/app/_service/themeHelper.service.ts
+++ guineapigcare.client/src/app/_service/themeHelper.service.ts
@@ -1,33 +1,49 @@
 import { Injectable, OnInit } from '@angular/core';
 import { ThemeService } from '../_service/theme.service';
 
-
 @Injectable({
-    providedIn: 'root',
-  })
-
-export class ThemeHelper{
-
+  providedIn: 'root',
+})
+export class ThemeHelper {
   currentTheme: boolean | undefined = undefined;
+  images: string[] = [];
 
   constructor(public theme: ThemeService) {}
 
+  preloadBackgrounds() {
+    this.images = [
+      'assets/images/backgrounds/no-login/mainPage.jpg',
+      'assets/images/backgrounds/no-login/badProducts.jpg',
+      'assets/images/backgrounds/no-login/beforeBuyGuineaPig.jpg',
+      'assets/images/backgrounds/no-login/goodProducts.jpg',
+      'assets/images/backgrounds/no-login/guinea-pig.jpg',
+      'assets/images/backgrounds/no-login/login.jpg',
+      'assets/images/backgrounds/no-login/register.jpg',
+      'assets/images/backgrounds/guineaPigLayout.jpg',
+      'assets/images/backgrounds/userLayout.jpg',
+    ];
 
-  setTheme() : void{
+    this.images.forEach((src) => {
+      const img = new Image();
+      img.src = src;
+    });
+  }
+
+  setTheme(): void {
     this.theme.isLightTheme$.subscribe({
       next: (response) => {
-        this.currentTheme = response
+        this.currentTheme = response;
       },
       error: (error) => console.log(error),
     });
   }
-  setBackground(url: string){
+  setBackground(url: string) {
     document.body.style.backgroundImage = `url(${url})`;
-    document.body.style.minHeight = "100%";
-    document.body.style.backgroundSize = "cover";
-    document.body.style.backgroundRepeat = "no-repeat";
-    document.body.style.backgroundAttachment = "fixed";
-    document.body.style.backgroundPosition = "center";
-    document.body.style.transition = 'background-image 1s ease-in-out';
-}
+    document.body.style.minHeight = '100%';
+    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundRepeat = 'no-repeat';
+    document.body.style.backgroundAttachment = 'fixed';
+    document.body.style.backgroundPosition = 'center';
+    document.body.style.transition = 'background-image 0.6s ease-in-out';
+  }
 }
--- guineapigcare.client/src/app/app.component.ts
+++ guineapigcare.client/src/app/app.component.ts
@@ -1,28 +1,30 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { ThemeService } from './_service/theme.service';
+import { ThemeHelper } from './_service/themeHelper.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-
   currentTheme: boolean | undefined = undefined;
 
-  constructor(private http: HttpClient, private theme: ThemeService) {}
+  constructor(
+    private themeService: ThemeService,
+    private themeHelper: ThemeHelper
+  ) {}
 
   ngOnInit() {
     this.setTheme();
+    this.themeHelper.preloadBackgrounds();
   }
   setTheme() {
-    this.theme.isLightTheme$.subscribe({
-      next: response => {
+    this.themeService.isLightTheme$.subscribe({
+      next: (response) => {
         this.currentTheme = response;
       },
-      error: error => error
-    })
-    }
+      error: () => {},
+    });
   }
+}
